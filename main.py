96ae0217bd95f2e22db50e5706fa5a0e21ea2224
Steffen Andersen <steff@telenor.dk>
2026-03-10 14:46:02 +0100

    Finished the backend with everything moved to SQL - program is working now. Next step is front end

--- main.py
+++ main.py
@@ -29,43 +29,52 @@ def load_dinners_from_file(file_path):
 
     return dinners_from_file
 
-dinners = load_dinners_from_file(DINNER_LIST_FILE) # our dinner list
+def get_all_dinners_from_db():
+
+    with sqlite3.connect(DB_FILE) as connection:
+        cursor = connection.cursor()
+
+        cursor.execute("""
+            SELECT dinner_name FROM dinners
+        """)
+        rows = cursor.fetchall()
+
+    dinners = []
+
+    for row in rows:
+        dinners.append(row[0])
+
+    return dinners
 
 def choose_dinner(dinner_options): # function to return random dinner
     return random.choice(dinner_options)
 
-def log_event(event_type, person, dinner): # function to save dinner to our file
+def log_event(event_type, person, dinner):
 
     todaysDate = datetime.date.today().strftime("%d-%m-%Y")
 
-    connection = sqlite3.connect(DB_FILE)
-    cursor = connection.cursor()
-
-    cursor.execute("""
-        INSERT INTO dinner_log (date, event_type, person, dinner)
-        VALUES (?, ?, ?, ?)
-    """, (todaysDate, event_type, person, dinner))
-
-    connection.commit()
-    connection.close()
+    with sqlite3.connect(DB_FILE) as connection:
+        cursor = connection.cursor()
+        cursor.execute("""
+            INSERT INTO dinner_log (date, event_type, person, dinner)
+            VALUES (?,?,?,?)
+        """, (todaysDate, event_type, person, dinner))
 
 def read_dinner_log(): # function to read dinner log and clean it
 
-    connection = sqlite3.connect(DB_FILE)
-    cursor = connection.cursor()
-
-    cursor.execute("""
-        SELECT date, event_type, person, dinner
-        FROM dinner_log
-    """)
-
-    rows = cursor.fetchall()
-
-    connection.close()
+    with sqlite3.connect(DB_FILE) as connection:
+        cursor = connection.cursor()
+        cursor.execute("""
+            SELECT date, event_type, person, dinner
+            FROM dinner_log
+        """)
+        rows = cursor.fetchall()
 
     return rows
 
 def get_available_dinners():
+
+    dinners = get_all_dinners_from_db()
     
     used_dinners = get_used_dinners_today()
 
@@ -130,9 +139,9 @@ def get_last_n_final_dinners(n = 5):
 
     last_5_dinners = []
 
-    log_file = read_dinner_log()
+    log_rows = read_dinner_log()
 
-    for line in log_file:
+    for line in log_rows:
 
         if len(line) != 4:
             continue
@@ -144,9 +153,9 @@ def get_last_n_final_dinners(n = 5):
 
 def get_most_recent_final_dinner():
 
-    log_file = read_dinner_log()
+    log_rows = read_dinner_log()
 
-    for line in reversed(log_file):
+    for line in reversed(log_rows):
 
         if len(line) != 4:
             continue
@@ -171,11 +180,11 @@ def print_last_n_final_dinner(n = 5):
 
 def get_session_events_for_date(date_str):
 
-    log_file = read_dinner_log()
+    log_rows = read_dinner_log()
 
     events = []
 
-    for line in log_file:
+    for line in log_rows:
         if len(line) == 4 and line[0] == date_str:
             events.append((line[1], line[2], line[3]))
 
@@ -324,21 +333,130 @@ def print_todays_status():
 
 def initialize_database():
 
-    connection = sqlite3.connect(DB_FILE)
-    cursor = connection.cursor()
+    with sqlite3.connect(DB_FILE) as connection:
+        cursor = connection.cursor()
 
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS dinner_log(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            date TEXT,
-            event_type TEXT,
-            person TEXT,
-            dinner TEXT
-        )
-    """)
+        # create event log table
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS dinner_log(
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       date TEXT,
+                       event_type TEXT,
+                       person TEXT,
+                       dinner TEXT
+                       )
+        """)
 
-    connection.commit()
-    connection.close()
+
+        # create dinner list table
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS dinners(
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       dinner_name TEXT UNIQUE
+                       )
+        """)
+
+        # load dinners from file
+        dinners = load_dinners_from_file(DINNER_LIST_FILE)
+
+        # insert dinners into the table - duplicates are ignored with OR IGNORE code
+        for dinner in dinners:
+            cursor.execute("""
+                INSERT OR IGNORE INTO dinners (dinner_name)
+                VALUES (?)
+            """, (dinner,))
+
+def get_dinner_statistics():
+
+    with sqlite3.connect(DB_FILE) as connection:
+        cursor = connection.cursor()
+        cursor.execute("""
+            SELECT dinner, COUNT(*)
+            FROM dinner_log
+            WHERE event_type = ?
+            GROUP BY dinner
+            ORDER BY COUNT(*) DESC
+        """, (FINAL,))
+
+        dinner_stats = cursor.fetchall()
+    
+    return dinner_stats
+
+def print_dinner_statistics():
+
+    dinner_stats = get_dinner_statistics()
+
+    print("Dinner statistics:")
+    print("")
+
+    if len(dinner_stats) == 0:
+        print("No dinner stats to show.")
+        return
+
+    for dinner, count in dinner_stats:
+        print(f"{dinner}: {count}")
+
+def show_all_dinners():
+    
+    dinners = get_all_dinners_from_db()
+
+    print("Available dinners:")
+    print("")
+
+    for number, dinner in enumerate(dinners, start = 1):
+        print(f"{number}. {dinner}")
+
+
+def add_dinner_to_list():
+
+    dinners = get_all_dinners_from_db()
+
+    user_input_dinner = input("What dinner would you like to add to the list?")
+    user_input_dinner = user_input_dinner.strip()
+
+    if len(user_input_dinner) == 0:
+        print("Empty input - please write a dinner.")
+        return
+
+    for dinner in dinners:
+        if user_input_dinner == dinner:
+            print(f"{user_input_dinner} is already in the dinner list.")
+            return
+    
+    with sqlite3.connect(DB_FILE) as connection:
+        cursor = connection.cursor()
+        cursor.execute("""
+            INSERT OR IGNORE INTO dinners (dinner_name)
+            VALUES (?)
+        """, (user_input_dinner,))
+
+    print("")
+    print(f"{user_input_dinner} has been added to the list.")
+
+def remove_dinner_from_list():
+
+    dinners = get_all_dinners_from_db()
+
+    for number, dinner in enumerate(dinners, start = 1):
+        print(f"{number}. {dinner}")
+    
+    remove_number = int(input("Input the number of what dinner you want to remove: "))
+    remove_index = remove_number - 1
+
+    if remove_number < 1 or remove_number > len(dinners):
+        print("Invalid number - please enter a number from the list.")
+        return
+
+    removed_dinner = dinners[remove_index]
+
+    with sqlite3.connect(DB_FILE) as connection:
+        cursor = connection.cursor()
+        cursor.execute("""
+            DELETE FROM dinners
+            WHERE dinner_name = ?
+        """, (removed_dinner,))
+
+    print(f"{removed_dinner} has been removed from the list.")
 
 # -----------------
 # MAIN PROGRAM FLOW
@@ -357,7 +475,7 @@ def main():
         print_todays_status()
         print("")
 
-        choice = input("Press 1 to spin, 2 for Steffen respin, 3 for Sabrina respin, 4 to finalize dinner or 0 to exit: ")
+        choice = input("Press 1 to spin, 2 for Steffen respin, 3 for Sabrina respin, 4 to finalize dinner, 5 to show dinner statistics, 6 to show all dinners in the list, 7 to add a dinner, 8 to remove a dinner from the list or 0 to exit: ")
 
         if choice == "1":
             success, message, dinner = action_spin()
@@ -392,11 +510,19 @@ def main():
             if success:
                 print(f"Dinner has been finalized: {dinner}")
                 break
+        elif choice == "5":
+            print_dinner_statistics()
+        elif choice == "6":
+            show_all_dinners()
+        elif choice == "7":
+            add_dinner_to_list()
+        elif choice == "8":
+            remove_dinner_from_list()
         elif choice == "0":
             print("Exiting program.")
             break
         else:
-            print("Invalid choise - please enter 0, 1, 2, 3 or 4.")
+            print("Invalid choice - please enter 0, 1, 2, 3, 4, 5, 6, 7 or 8.")
         
         
 
@@ -404,3 +530,6 @@ def main():
 # this starts the program
 if __name__ == "__main__":
     main()
+
+#if __name__ == "__main__":
+#    add_dinner_to_list()
